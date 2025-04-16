226f55c09c3deca4f45aa4328d9f6ee502bd3876
imarag <giannis.marar@hotmail.com>
2025-04-17 00:48:15 +0300

    finished the home page

--- kauk-app/src/js/main.js
+++ kauk-app/src/js/main.js
@@ -1,24 +1,44 @@
-const burgerButton = document.querySelector("#burger-button");
-const mainNav = document.querySelector("#main-nav");
-const themeToggleInput = document.querySelector('#theme-container input');
-const htmlElement = document.documentElement;
+document.addEventListener("DOMContentLoaded", () => {
+    const menuButtonController = document.querySelector("#menu-button-controller");
+    const mainNav = document.querySelector("#main-nav");
+    const languageToggleInput = document.querySelector('#language-container input');
+    let langEnElements = document.querySelectorAll('[data-lang="en"]');
+    let langGrElements = document.querySelectorAll('[data-lang="gr"]');
+    const currentLanguage = localStorage.getItem("language") || "gr";
 
-const themes = {
-    light: "light",
-    dark: "synthwave"
-}
-const currentTheme = localStorage.getItem("theme") || themes["light"]
-if (currentTheme === themes["dark"]) {
-    themeToggleInput.checked = true
-}
-htmlElement.setAttribute("data-theme", currentTheme);
+    function enableGreek() {
+        langEnElements.forEach(el => el.style.display = 'none')
+        langGrElements.forEach(el => el.style.display = '')
+    }
 
-themeToggleInput.addEventListener("change", () => {
-    const themeValue = htmlElement.getAttribute("data-theme") === themes["light"] ? themes["dark"] : themes["light"];
-    htmlElement.setAttribute("data-theme", themeValue);
-    localStorage.setItem("theme", themeValue);
+    function enableEnglish() {
+        langEnElements.forEach(el => el.style.display = '')
+        langGrElements.forEach(el => el.style.display = 'none')
+    }
+
+    if (currentLanguage === "en") {
+        languageToggleInput.checked = true
+        enableEnglish()
+    }
+    else {
+        languageToggleInput.checked = false
+        enableGreek()
+    }
+
+    languageToggleInput.addEventListener("change", () => {
+        if (languageToggleInput.checked) {
+            localStorage.setItem("language", "en");
+            enableEnglish()
+        }
+        else {
+            localStorage.setItem("language", "gr");
+            enableGreek()
+        }
+    });
+
+    menuButtonController.checked = false;
+
+    menuButtonController.addEventListener("change", () => {
+        mainNav.classList.toggle("show-nav")
+    })
 });
-
-burgerButton.addEventListener("click", () => {
-    mainNav.classList.toggle("show-nav")
-})
